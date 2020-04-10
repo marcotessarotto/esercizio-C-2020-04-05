e0dab6c2257493080c8de2209df116723561bae4
Spopoi <61315723+Spopoi@users.noreply.github.com>
2020-04-10 17:05:22 +0200

    Update esercizio-C-2020-04-05.c
    first commit

--- src/esercizio-C-2020-04-05.c
+++ src/esercizio-C-2020-04-05.c
@@ -4,5 +4,26 @@
  *  Created on: Apr 2, 2020
  *      Author: marco
  */
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+	char inputChar;
+	int arrayLength = 5;
+	char *char_array = calloc(arrayLength, sizeof(char));
+	int index = 0;
+	while ((inputChar = getchar()) != EOF) {
+		if (index == arrayLength - 1) {
+			arrayLength = arrayLength * 2;
+			if (realloc(char_array, arrayLength) == NULL) {
+				perror("realloc error!");
+				exit(EXIT_FAILURE);
+			}
+		}
+		char_array[index]=inputChar;
+		index++;
+	}
+	free(char_array);
+	return 0;
+}
 

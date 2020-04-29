bb5159d0498f2ef6d4f06bf2c2db9a117c8fc7bd
utente <utente@debian10.units.it>
2020-04-29 21:04:12 +0200

    completo

--- esercizio-C-2020-04-05/esercizio-C-2020-04-05.c
+++ esercizio-C-2020-04-05/esercizio-C-2020-04-05.c
@@ -1,13 +1,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
-
-#include <errno.h>
 
 int contarighe=0;
 
